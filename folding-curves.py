d13847b5b77f8fc17c4ed78cb760e81e1fca8f39
Jeff Stevens <jefste@gmail.com>
2015-02-16 15:29:46 -0500

    Add curve fit for linear and show a plot

--- folding-curves.py
+++ folding-curves.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 
 from scipy.optimize import fsolve
+from scipy.optimize import curve_fit
 from math import exp
 
 def fit_folded(x, c_f,m_f,c_u,m_u,m_g,d_g):
@@ -24,8 +25,30 @@ test_data_x=(0,0.5,0.94,1.33,1.51,1.68,1.85,2,2.15,2.29,2.42,2.55,2.67,2.78,2.89
         
 test_data_y=(-1573.86424,-373.92125,98.63756,1930.82677,2774.18611,5095.27712,12323.59871,19510.18524,31674.4006,53568.05441,80023.81096,115925.6316,150256.8982,177651.9872,197236.1776,208099.1024,215700.6272,240268.0321,250882.5655,262338.8597,290747.8803)
 
-plt.plot(test_data_x,test_data_y,'g')
-plt.plot(test_data_x,test_data_y,'ro')
+#need to find method to fit and return all parameters
+## use those parameters to generate a fit plot, fold baseline and unfold baseline
+
+#try to use a linear model first for proof of principal
+def test_f(x, m, b):
+    return x*m+b
+#use curve_fit on the test data
+popt,pcov = curve_fit(test_f,test_data_x,test_data_y)
+#make an array of 100 points
+xfit=numpy.linspace(test_data_x[0],test_data_x[-1],100)
+#create array using the fit parameters
+test_fit_y=test_f(xfit, *popt)
 
 
+#plot data
+plt.plot(test_data_x,test_data_y,marker='o')
+#plot fit
+plt.plot(xfit,test_fit_y,ls='-')
+#show data
 plt.show()
+
+
+#need to put fold baseline on plot
+#need to put unfold baseline on plot
+#need to put fit on plot
+
+
